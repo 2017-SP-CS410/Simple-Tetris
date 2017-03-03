d48fdae40dd6afcebd385480eefa1557314e13f7
Asher Holloman <asherholloman@gmail.com>
2017-03-03 14:26:42 -0700

    filled in the methods

--- Tetris/src/tetris/GamePlay.java
+++ Tetris/src/tetris/GamePlay.java
@@ -5,17 +5,53 @@
  */
 package tetris;
 
+import java.awt.Color;
+import java.util.Random;
+
 /**
  *
  * @author Brendan
  */
 public class GamePlay {
+    Random r = new Random();
+    int rand = r.nextInt(7);
+    
+    
+    
     public void updateLevel(GameState gs, Integer lines) {
         // Update the level in gs based on the number of lines cleared
         // Update the gravity based the current level
+        if(lines > 0){
+            gs.level++;
+            gs.gravity += 0.02*gs.gravity;
+        }
     }
     public void nextTet(GameState gs) {
         // Abstract Factory
         // Randomly Generates a new Tetromino using the Static Factories inside Tetromino
+        P2 p = new P2(130, 0);
+        Color c = Color.BLACK;
+            switch(rand){
+                case '0': 
+                    gs.currentTet = Tetromino.tetI(p, c);
+                    break;
+                case '2':
+                    gs.currentTet = Tetromino.tetL(p, c);
+                    break;
+                case '3':
+                    gs.currentTet = Tetromino.tetS(p, c);
+                    break;
+                case '4':
+                    gs.currentTet = Tetromino.tetZ(p, c);
+                    break;
+                case '5':
+                    gs.currentTet = Tetromino.tetO(p, c);
+                    break;
+                case '6': 
+                    gs.currentTet = Tetromino.tetJ(p, c);
+                    break;
+            } 
+    
+    
     }
 }
