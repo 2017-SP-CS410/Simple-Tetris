e4ed457d092cf4b607d42e77ae72ac962daaa308
lmgllc <lukuscannon@gmail.com>
2017-03-03 15:08:07 -0700

    coverage
    updated P2 class with coverage

--- Tetris/src/tetris/P2.java
+++ Tetris/src/tetris/P2.java
@@ -10,15 +10,18 @@ package tetris;
  * @author Brendan
  */
 public class P2 {
+    public static boolean coverage[] = new boolean[2];
     // This is screen coordinates
     public double x, y;
     
     public P2(double x, double y) {
+        coverage[0] = true;
         this.x = x;
         this.y = y;
     }
     
     public P2 add(P2 other) {
+        coverage[1] = true;
         double nx = this.x + other.x;
         double ny = this.y + other.y;
         return new P2(nx, ny);
